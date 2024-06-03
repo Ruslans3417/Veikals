c9590b732ab0bf3678828dad413f9d3dc059673e
Ruslans3417 <124033222+Ruslans3417@users.noreply.github.com>
2024-06-03 13:29:43 +0300

    Update edit_product.php

--- ekz/edit_product.php
+++ ekz/edit_product.php
@@ -11,11 +11,11 @@ if (isset($_GET['id'])) {
     $product = $result->fetch_assoc();
 
     if (!$product) {
-        echo "Товар не найден";
+        echo "Produkts nav atrasts";
         exit();
     }
 } else {
-    echo "Неверный запрос.";
+    echo "Nederīgs pieprasījums.";
     exit();
 }
 ?>
@@ -25,34 +25,34 @@ if (isset($_GET['id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Редактировать товар</title>
+    <title>Rediģēt produktu</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container my-4">
-        <h1 class="text-center">Редактировать товар</h1>
+        <h1 class="text-center">Rediģēt produktu</h1>
         <form action="update_product.php" method="post" enctype="multipart/form-data">
             <input type="hidden" name="id" value="<?php echo $product['id']; ?>">
             <div class="form-group">
-                <label for="productName">Название товара</label>
+                <label for="productName">produkta nosaukums</label>
                 <input type="text" class="form-control" id="productName" name="productName" value="<?php echo $product['name']; ?>" required>
             </div>
             <div class="form-group">
-                <label for="productPrice">Цена товара</label>
+                <label for="productPrice">Produkta cena</label>
                 <input type="text" class="form-control" id="productPrice" name="productPrice" value="<?php echo $product['price']; ?>" required>
             </div>
             <div class="form-group">
-                <label for="productDescription">Описание товара</label>
+                <label for="productDescription">Produkta apraksts</label>
                 <textarea class="form-control" id="productDescription" name="productDescription" rows="3" required><?php echo $product['description']; ?></textarea>
             </div>
             <div class="form-group">
-                <label for="productImage">Изображение товара</label>
+                <label for="productImage">Produkta attēls</label>
                 <input type="file" class="form-control-file" id="productImage" name="productImage">
                 <?php if (!empty($product['image'])): ?>
                     <img src="foto/<?php echo $product['image']; ?>" alt="<?php echo $product['name']; ?>" style="width: 100px;">
                 <?php endif; ?>
             </div>
-            <button type="submit" class="btn btn-primary">Сохранить изменения</button>
+            <button type="submit" class="btn btn-primary">Saglabāt izmaiņas</button>
         </form>
     </div>
 
